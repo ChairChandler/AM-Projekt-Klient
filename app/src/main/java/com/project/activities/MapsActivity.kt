9d084754945f0d25430fb619f5be0058a6fd32a5
Adam Lewandowski <razzors1998@gmail.com>
2020-08-03 16:04:11 +0200

    Added ico to info button.

--- app/src/main/java/com/project/activities/MapsActivity.kt
+++ app/src/main/java/com/project/activities/MapsActivity.kt
@@ -7,17 +7,12 @@ import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -28,15 +23,13 @@ import com.google.android.gms.maps.model.*
 import com.project.HospitalManager
 import com.project.R
 import com.project.models.Hospital
-import org.json.JSONArray
-import org.json.JSONObject
-import kotlin.math.pow
-import kotlin.math.sqrt
+import kotlinx.android.synthetic.main.activity_maps.*
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener,
+    GoogleMap.OnInfoWindowCloseListener {
 
-    private val apiKey: String = R.string.google_maps_key.toString()
     private val hospitalsPositionMap: HashMap<LatLng, Hospital> = HashMap()
+    private lateinit var clickedMarker: Marker
 
     private lateinit var mMap: GoogleMap
     private lateinit var userLocalisation: Location
@@ -59,13 +52,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        infoButton.setOnClickListener { _ ->
+            val intent = Intent(this, HospitalInfoActivity::class.java)
+            val selectedHospitalInfo = hospitalsPositionMap[clickedMarker!!.position]
+            intent.putExtra("info", selectedHospitalInfo)
+            startActivity(intent)
+        }
+
         locationCallback = object : LocationCallback() {
             override fun onLocationResult(p0: LocationResult) {
                 super.onLocationResult(p0)
                 userLocalisation = p0.lastLocation
             }
         }
-        //HospitalManager.addComment("Komentarz", "Ala", HospitalManager.SAMPLE_HOSPITAL_ID)
 
         for (info in HospitalManager.downloadHospitalData()) {
             HospitalManager.hospitals.add(info)
@@ -90,6 +89,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
 
         mMap.uiSettings.isZoomControlsEnabled = true
         mMap.setOnMarkerClickListener(this)
+        mMap.setOnInfoWindowCloseListener(this)
 
         fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
             return ContextCompat.getDrawable(context, vectorResId)?.run {
@@ -107,8 +107,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         for (hospital in HospitalManager.hospitals) {
             val place = LatLng(hospital.location.lat, hospital.location.lng)
             hospitalsPositionMap[place] = hospital
-            //val icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)
-            mMap.addMarker(MarkerOptions().position(place).icon(icon))
+            mMap.addMarker(
+                MarkerOptions().position(place).title(hospital.name).snippet(hospital.description)
+                    .icon(icon)
+            )
         }
 
         this.setUpMap()
@@ -140,72 +142,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
             }
         }
-    }
-
-    override fun onMarkerClick(p0: Marker?): Boolean {
-        val selectedHospitalInfo = hospitalsPositionMap[p0?.position]
-
-        AlertDialog.Builder(this)
-            .setMessage("${selectedHospitalInfo?.description}\n\nWhat do you want to know?")
-            .setTitle(selectedHospitalInfo?.name)
-            .setPositiveButton(
-                "Information"
-            ) { _, _ ->
-                val intent = Intent(this, HospitalInfoActivity::class.java)
-
-                intent.putExtra("info", selectedHospitalInfo)
-                startActivity(intent)
-            }.setNegativeButton(
-                "Route"
-            ) { _, _ ->
-                var from = LatLng(userLocalisation.latitude, userLocalisation.longitude)
-                //var from = LatLng(p0!!.position.latitude - 20, p0!!.position.longitude);
-                //var to = p0!!.position
-                var to = LatLng(37.773972, -120.431297)
-                this.createRoute(from, to)
-            }.show()
-        return true
-    }
-
-    private fun createRoute(from: LatLng, to: LatLng) {
-        val path: MutableList<List<LatLng>> = ArrayList()
-        val urlDirections =
-            "http://www.yournavigation.org/api/1.0/gosmore.php?flat=${from.latitude}&flon=${longitudeArgCorrection(
-                from.longitude
-            )}&tlat=${to.latitude}&tlon=${longitudeArgCorrection(to.longitude)}&format=geojson"
-        println(urlDirections)
-        val directionsRequest = object :
-            StringRequest(Request.Method.GET, urlDirections, Response.Listener<String> { response ->
-                val jsonResponse = JSONObject(response)
-                // Get routes
-                val routes = jsonResponse.getJSONArray("coordinates")
-                for (i in 0 until routes.length()) {
-                    val latLngArray = routes[i] as JSONArray
-                    val latLng = LatLng(latLngArray.get(0) as Double, latLngArray.get(1) as Double)
-                    path.add(listOf(latLng))
-                }
-                for (i in 0 until path.size) {
-                    mMap!!.addPolyline(PolylineOptions().addAll(path[i]).color(Color.RED))
-                }
-                mMap!!.animateCamera(
-                    CameraUpdateFactory.newLatLngZoom(
-                        LatLng(
-                            path[0][0] as Double,
-                            path[0][1] as Double
-                        ), 12f
-                    )
-                )
-            }, Response.ErrorListener { _ ->
-            }) {}
-        val requestQueue = Volley.newRequestQueue(this)
-        requestQueue.add(directionsRequest)
-    }
-
-    private fun longitudeArgCorrection(longitude: Double): String =
-        if (longitude < 0) "\\\\${longitude}" else longitude.toString()
-
-    private fun calcDistance(a: LatLng, b: LatLng): Double {
-        return sqrt((a.latitude - b.latitude).pow(2) + (a.longitude - b.longitude).pow(2))
     }
 
     private fun startLocationUpdates() {
@@ -283,5 +219,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         if (!locationUpdateState) {
             startLocationUpdates()
         }
+    }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        infoButton.visibility = View.VISIBLE
+        clickedMarker = p0!!
+        return false
+    }
+
+    override fun onInfoWindowClose(p0: Marker?) {
+        infoButton.visibility = View.INVISIBLE
     }
 }
